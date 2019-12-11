f8ba94d8de875adfd83f82619c4c58c811acd94b
Devin Shields <devinshields@likelystory.ca>
2019-12-11 15:26:30 -0500

    Added CSS for mobile, widescreen.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Initialize and add map //
+// Initialize and add map
 function initMap() {
     const loc = { lat: 42.361145, lng: -71.057083 };
     const map = new google.maps.Map(document.querySelector('.map'),
@@ -9,3 +9,28 @@ function initMap() {
 
     const marker = new google.maps.Marker({ position: loc, map: map});
 }
+
+// Sticky menu opacity on scroll
+window.addEventListener('scroll', function() {
+    if (this.window.scrollY > 150) {
+        this.document.querySelector('#navbar').style.opacity = 0.85;
+    } else {
+        this.document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+//Smoth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -65
+            },
+            800
+        );
+    }
+});
